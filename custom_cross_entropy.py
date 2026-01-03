88f20a31c82223c50f6c1e1abeaac22067efcc9c
Nathan Truong <nathanngtruong@gmail.com>
2026-01-03 02:46:44 -0800

    Update custom_cross_entropy.py

--- custom_cross_entropy.py
+++ custom_cross_entropy.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn.functional as F
 
 
+
 def cross_entropy_loss(
     logits,
     labels,
     perp_values: torch.Tensor = None,
     perp_indices: torch.Tensor = None, #should be rank 1 tensor
+    lookup_surrogate_to_self_tokens: torch.Tensor = None,
     ignore_index: int = -100,
     reduction: str = "mean",
     compute_z_loss: bool = False,
@@ -21,12 +23,15 @@ def cross_entropy_loss(
         gathered_nll = -torch.log(gathered_logit_probs + 1e-10) # (batch_len, seq_len, k)
         weighted_loss_tensor = gathered_nll * F.softmax(-perp_values, dim=-1) 
         surr_loss_term = torch.sum(weighted_loss_tensor, dim=-1) #(batch_len, seq_len) keepdim should be false automatically.
-
+        
+        #translate back
+        stacked_translation_tensor = lookup_surrogate_to_self_tokens.repeat((batch_size, len(lookup_surrogate_to_self_tokens)))
+        translated_labels = torch.gather(input=stacked_translation_tensor, dim=1, index=labels)
         
         
-        loss = surr_loss_term.sum() + F.cross_entropy(logits=logits, labels=labels, ignore_index=ignore_index, reduction='sum') #reduction='none' returns tensor with losses. shoudl be in same shape as (batch_size, seq)
+        loss = surr_loss_term.sum() + F.cross_entropy(logits=logits, labels=translated_labels, ignore_index=ignore_index, reduction='sum') #reduction='none' returns tensor with losses. shoudl be in same shape as (batch_size, seq)
         if reduction == "mean":
-            loss /= ((labels != ignore_index).sum().item() + (batch_size * seq_len * k))
+            loss /= ((translated_labels != ignore_index).sum().item() + (batch_size * seq_len * k))
             
 
         if not compute_z_loss:
