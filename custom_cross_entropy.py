7c0a070452c57fb58024361c01bd7e81caeb9fc9
Nathan Truong <nathanngtruong@gmail.com>
2026-01-02 17:58:32 -0800

    Update custom_cross_entropy.py

--- custom_cross_entropy.py
+++ custom_cross_entropy.py
@@ -1,28 +1,47 @@
 import torch
 import torch.nn.functional as F
 
+
 def cross_entropy_loss(
     logits,
     labels,
-    weighted_perp_loss_tensor, #should be rank 1 tensor
+    perp_indices: torch.Tensor = None, #should be rank 1 tensor
     ignore_index: int = -100,
     reduction: str = "mean",
     compute_z_loss: bool = False,
     z_loss_multiplier: float = 1e-4,
 ):
-    loss = torch.sum(weighted_perp_loss_tensor) + F.cross_entropy(logits=logits, labels=labels, ignore_index=ignore_index, reduction='sum') 
-    if reduction == "mean":
-        loss /= torch.mean(((labels == ignore_index).sum().item() + len(weighted_perp_loss_tensor)))
+    if perp_indices is not None:        
+        #we use torch.gather with the perp_indices as indices (batch_size, seq_len - 1, 4). We use the output logits of shape (batch_size, seq_len - 1, vocab) 
+        gathered_auxiliary_loss_terms = torch.gather(logits, dim=2, index=perp_indices)
+        loss = torch.sum(weighted_perp_loss_tensor) + F.cross_entropy(logits=logits, labels=labels, ignore_index=ignore_index, reduction='none') #reduction='none' returns tensor with losses. shoudl be in same shape as (batch_size, seq)
+        if reduction == "mean":
+            loss /= ((labels != ignore_index).sum().item() + len(weighted_perp_loss_tensor))
 
-    if not compute_z_loss:
-        return loss, None
+        if not compute_z_loss:
+            return loss, None
 
-    z_squared = logits.logsumexp(-1).pow(2) #we can keep this, doesn't do anything harmful
-    if reduction == "mean":
-        z_squared = (z_squared * (labels != ignore_index)).mean()
-    elif reduction == "sum":
-        z_squared = (z_squared * (labels != ignore_index)).sum() # I will not care for this portion. It is not in my project
+        z_squared = logits.logsumexp(-1).pow(2) #we can keep this, doesn't do anything harmful
+        if reduction == "mean":
+            z_squared = (z_squared * (labels != ignore_index)).mean()
+        elif reduction == "sum":
+            z_squared = (z_squared * (labels != ignore_index)).sum() # I will not care for this portion. It is not in my project
 
-    z_loss = z_loss_multiplier * z_squared
+        z_loss = z_loss_multiplier * z_squared
 
-    return loss, z_loss
+        return loss, z_loss
+    
+    else:
+        
+        loss = F.cross_entropy(logits, labels, ignore_index=ignore_index, reduction=reduction)
+
+        if not compute_z_loss:
+            return loss, None
+
+        z_squared = logits.logsumexp(-1).pow(2)
+        if reduction == "mean":
+            z_squared = (z_squared * (labels != ignore_index)).mean()
+        elif reduction == "sum":
+            z_squared = (z_squared * (labels != ignore_index)).sum()
+
+        z_loss = z_loss_multiplier * z_squared
